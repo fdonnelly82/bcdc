25540228ec62a7c48e44db30a4792d84cdfd173f
Krzysztof Bracha <duraven@poczta.onet.pl>
2016-03-30 12:53:12 +0100

    Merged browse_all.html into index.html Worked on the overall styling Added footer

--- js/scroll.js
+++ js/scroll.js
@@ -8,11 +8,16 @@ var onScroll = function(event) {
     $("#nav-search").removeClass("active");
     $("#nav-compare").removeClass("active");
     $("#nav-featured").removeClass("active");
+    $("#nav-browse-all").removeClass("active");
 
     var scrollTop = $(window).scrollTop();
 
-    if(scrollTop >=  $("#featured").offset().top - $("#featured").height() * 0.60)
-    { /* highlight featured when 40% becomes visible */
+    if(scrollTop >=  $("#browse_all").offset().top - $("#browse_all").height() * 0.50)
+    { /* highlight browse all when 50% becomes visible */
+        $("#nav-browse-all").addClass("active");
+    }
+    else if(scrollTop >=  $("#featured").offset().top - $("#featured").height() * 0.50)
+    { /* highlight featured when 50% becomes visible */
         $("#nav-featured").addClass("active");
     }
     else
