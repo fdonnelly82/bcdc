27c0bf048840e2768a4003af3b211694588700d1
Krzysztof Bracha <duraven@poczta.onet.pl>
2016-04-21 20:11:25 +0100

    Finished Cineworld API

--- js/belmont.js
+++ js/belmont.js
@@ -41,69 +41,6 @@ var belmontFetchMovies = function(callback)
     }
 
     yqlUrlQuery("http://www.belmontfilmhouse.com/new-releases/","belmontFetchMoviesCallback");
-    /*
-    callbackFunc = function(response)
-    {
-        var html = yqlResponseToHTML(response);
-
-        var movie_hyperlink = html.find('[id^=dnn_ctr1418_ViewCinemaListing_MKII_rptSynopsis_hlInfo_]');
-        var movie_img = html.find('[id^=dnn_ctr1418_ViewCinemaListing_MKII_rptSynopsis_imgFilmImage_]');
-        var movie_div = html.find('[id^=dnn_ctr1418_ViewCinemaListing_MKII_rptSynopsis_divFilm_]');
-
-        var movie_session = [];
-        for(i = 0; i < movie_div.length; i++)
-        {
-            movie_date = $(movie_div[i]).find('[class^=sysnopsisFullDate]');
-            movie_session[i] = [];
-
-            for(j = 0; j < movie_date.length; j++)
-            {
-                var session = {
-                    date : $(movie_date[j]).text().trim(),
-                    projection : [],
-                };
-
-                var session_date_details = $(movie_date[j]).next().find("a");
-
-                for(k = 0; k < session_date_details.length; k++)
-                {
-                    session.projection[k] = {
-                        time : $(session_date_details[k]).children().first().text(),
-                        type : $(session_date_details[k]).attr("data-attribute"),
-                        url : $(session_date_details[k]).attr("href")
-                    };
-                }
-
-                movie_session[i].push(session);
-            }
-        }
-
-        var movie = [];
-        for(var i = 0; i < movie_hyperlink.length; i++)
-        {
-            movie.push({
-                name: $(movie_hyperlink[i]).text(),
-                simpleName: $(movie_hyperlink[i]).text().toLowerCase().replace(","," "),
-                url: "https://www.myvue.com" + $(movie_hyperlink[i]).attr('href'),
-                thumbnail: "https://www.myvue.com" + $(movie_img[i]).attr("src"),
-                session : movie_session[i]
-            });
-
-            movie[movie[i].name] = movie[i];
-
-            for(var j = 0; j < movie[i].session.length; j++){
-                movie[i].session[movie[i].session[j].date] = movie[i].session[j];
-            }
-        }
-
-        callback(movie);
-
-    }
-
-    //yqlUrlQuery("https://www.myvue.com/latest-movies/view/all-times","callbackFunc",true);
-    yqlUrlQuery("http://www.myvue.com/latest-movies/cinema/aberdeen?__EVENTTARGET=dnn%24ctr1418%24ViewCinemaListing_MKII%24lbFull&__EVENTARGUMENT=","callbackFunc",true);
-
-     */
 }
 
 
@@ -192,6 +129,8 @@ var belmontFetchProjections = function(movie,callback)
 }
 
 
+
+/*
 var belmontGetProjectionPricing = function(projection_url, callback)
 {
     belmontProjectionPricingCallback = function(response)
@@ -200,37 +139,9 @@ var belmontGetProjectionPricing = function(projection_url, callback)
 
         console.log(html);
 
-        return;
-        var booking_div = html.find('.booking_seat_info');
-        var price = [];
-
-        for(var i = 0; i < booking_div.length; i++)
-        {
-            $(booking_div[i]).children().first().remove();
-            price.push($(booking_div[i]).text().trim().substr(4));
-        }
-
-        var projectionPricing = {
-            adult : {
-                standard : price[0],
-                vip : price[1],
-            },
-            senior : {
-                standard : price[2],
-                vip : price[3],
-            },
-            student : {
-                standard : price[4],
-                vip : price[5],
-            },
-            teen : {
-                standard : price[6],
-                vip : price[7],
-            }
-        }
-
         callback(projectionPricing);
     }
 
     yqlUrlQuery(projection_url,"belmontProjectionPricingCallback","json",true);
 }
+*/
