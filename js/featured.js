02618eca21b4e75c33ae824c0eba3bb6c2dcf6bc
1302264 <fdonnelly82@gmail.com>
2016-04-22 15:50:37 +0100

    successfully calls one poster, image size needs to be fixed

--- js/featured.js
+++ js/featured.js
@@ -1,37 +1,35 @@
-var getFeatured = function(callback) {
-        $(document).ready(function() {
-            $('a.retrieve').click(function() {
-                $.ajax({
-                    url: 'http://www.cineworld.com/api/quickbook/films',
-                    type: 'GET',
-                    data: {key: 'external', full: true, cinema: 33},
-                    dataType: 'jsonp', // Setting this data type will add the callback parameter for you
-                    success: parseFilms
-                });
-            });
-
-            $('a.clear').click(function() {
-                $('span.film.count').text('0');
-                $('ol.film.list').empty();
-            });
+var getFeatured = function() {
+    //when page loads, film name, certificate and poster loads with it
+    $(document).ready(function(){
+        //ajax to connect to cineworld api to get film data
+        $.ajax({
+            url: 'http://www.cineworld.com/api/quickbook/films',
+            type: 'GET',
+            data: {key: 'qUnEyRXt', full: true, cinema: 1},
+            dataType: 'jsonp',
+            success: parseFilms
         });
+    });
 
-        function parseFilms(response, status) {
-            var html = '';
-
-            // Check for errors from the server
-            if (response.errors) {
-                $.each(response.errors, function () {
-                    html += '<2>' + this + '</2>';
+    function parseFilms(response, status){
+        //variable to store html responses
+        var html = '';
+        //returns error message if cineworld server can't be reached
+        if (response.errors) {
+            $.each(response.errors, function () {
+                html += '<h1>' + this + '</h1>';
+            });
+            //takes in film name, classification, poster url and places them in appropriate tags
+        } else {
+            $.each(response.films, function () {
+                html += '<p>' + this.title + ' (' + this.classification + ')</p>';
+                //shows one poster
+                html += $('img').attr("src", this.poster_url).load(function(){
+                    $(this).width.height.appendTo('img');
                 });
-            } else {
-                $('span.film.count').text(response.films.length);
-                $.each(response.films, function () {
-                    html += '<h2>' + this.title + ' (' + this.classification + ')</h2>';
-                });
-            }
-
-            // Faster than doing a DOM call to append each node
-            $('ol.film.list').append(html);
+            });
         }
+        //appends all DOM calls at once
+        $('ol.film.list').append(html);
+    }//end of function
 }
